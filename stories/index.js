43050740d392c4870dc130f8ecd3fe30cec2f6d9
Christoffer Åström <cam@qlik.com>
2019-05-27 13:20:30 +0200

    chore: use @nebula.js/ui/components

--- stories/index.js
+++ stories/index.js
@@ -1,15 +1,46 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { muiTheme } from 'storybook-addon-material-ui';
-import { Button } from '@storybook/react/demo';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import {
+  Button,
+  ButtonBase,
+  IconButton,
+  Grid,
+  Card,
+  CardContent,
+  Typography,
+  Popover,
+  TableRow,
+  TableCell,
+  Toolbar,
+  Divider,
+  Drawer,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  List,
+  ListSubheader,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  ListItemSecondaryAction,
+  FormGroup,
+  FormControlLabel,
+  Switch,
+  TextField,
+} from '@nebula.js/ui/components';
 
 storiesOf('Button', module)
   .addDecorator(muiTheme())
-  .add('with text', () => <Button>Hello Button</Button>)
+  .add('with text', () => <Button>Nebula Button</Button>)
   .add('with emoji', () => (
     <Button>
       <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
+        Nebula 😀 😎 👍 💯
       </span>
     </Button>
   ));
